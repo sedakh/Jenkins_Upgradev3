5bb6480fb88f847b99e9ac2aa1686e5a9c56dfbf
sedakh <62213415+sedakh@users.noreply.github.com>
2020-05-18 17:16:24 +0400

    fiexed error

--- maven-samples/single-module/src/test/java/com/example/TestGreeter.java
+++ maven-samples/single-module/src/test/java/com/example/TestGreeter.java
@@ -36,9 +36,10 @@ public class TestGreeter {
     String someone = "World, Thanks you making me Happy";
 
     assertThat(greeter.greet(someone).length(), is(greaterThan(someone.length())));
-    
+  }
+  
       @Test
-  public void greetShouldIncludeGreetingMessage() {
+  public void greetShouldIncludeGreetingMsg() {
     String someone = "Thanks >> SKH";
 
     assertThat(greeter.greet(someone).length(), is(greaterThan(someone.length())));
